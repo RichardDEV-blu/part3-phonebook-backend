6afbf322fe8d5a4492074ef782b71fc13a69943c
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-06 22:54:47 -0500

    part 3: 3.5

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 let persons = [
-  { 
-    id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
+    }
 ]
-app.get('/api/persons',(request,response)=>{
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000)
+}
+app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/api/persons/:id', (request,response)=>{
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const p = persons.find(p => p.id ===id)
-    if (p){
+    const p = persons.find(p => p.id === id)
+    if (p) {
         response.json(p)
     } else {
         response.status(404).end()
@@ -39,22 +44,33 @@ app.get('/api/persons/:id', (request,response)=>{
 
 })
 
-app.get('/info', (request,response)=>{
+app.get('/info', (request, response) => {
     const date = new Date()
     response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${date}</p>`)
 })
 
-app.delete('/api/persons/:id',(request,response)=>{
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(p => p.id !== id)
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 
 
 const PORT = 3001
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
