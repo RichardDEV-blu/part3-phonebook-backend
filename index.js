bba27e57612773e4ad8d4d3d43abaa30b3dd7d3e
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-09 14:31:42 -0500

    part 3: 3.15

--- index.js
+++ index.js
@@ -36,10 +36,12 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id).then(result => {
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
         response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
