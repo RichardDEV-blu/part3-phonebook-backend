8053fa9ecbb8b2ed74e60b50c58f631769a0fea6
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-09 14:39:47 -0500

    part 3: 3.16

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
         } else {
             response.status(404).end()
         }
-    })
+    }).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -62,6 +62,16 @@ app.post('/api/persons', (request, response) => {
         response.json(saved)
     })
 })
+
+const errorHandler = (error, req, res, next)=>{
+    console.error(error.message)
+    if(error.name === 'CastError'){
+        return res.status(400).send({error: 'malformatted id'})
+    }
+    next (error)
+
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
