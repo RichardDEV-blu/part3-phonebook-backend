3fa4a2fb9d60a509e11e0d89ba22f08a4c8e7801
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-11 11:13:45 -0500

    part 4: 4.2

---
+++ app.js
@@ -0,0 +1,35 @@
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
