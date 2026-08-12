cee78d4a7f01b530c28e652c774c388c731e9a30
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-12 09:38:00 -0500

    part 4: 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,29 @@
 const dummy = (blogs) => {
-return 1
+  return 1
 }
 
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog)=>{
-        return sum + blog.likes
-    },0)
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const maxIndex = blogs.reduce((accumulatorIndex, currentBlog, currentIndex, blogsArray) => {
+    return currentBlog.likes > blogsArray[accumulatorIndex].likes ? currentIndex : accumulatorIndex
+  }, 0)
+  const favorite = blogs[maxIndex]
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
 }
 
 module.exports = {
-    dummy,
-    totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
+
