d173e1ed492fe6f8fb1976d1f01f5ba95726622b
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-13 11:29:36 -0500

    part 4: 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -20,12 +20,35 @@ test('blogs are returned as JSON', async () => {
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
-test('blogs have an id property',async ()=>{
+test('blogs have an id property', async () => {
     const response = await api
         .get('/api/blogs')
 
-    assert(response.body.every(blog=>blog.id))
+    assert(response.body.every(blog => blog.id))
 
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'Testing blog',
+        author: 'Richard',
+        url: 'https://testingblog.com',
+        likes: 5
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+
+    assert.strictEqual(response.body.title, newBlog.title)
+    assert.strictEqual(response.body.author, newBlog.author)
+    assert.strictEqual(response.body.url, newBlog.url)
+    assert.strictEqual(response.body.likes, newBlog.likes)
+
+    const blogs = await api.get('/api/blogs')
+
+    assert.strictEqual(blogs.body.length, helper.initialBlogs.length + 1)
 })
 
 after(async () => {
