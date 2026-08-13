08803e8fc4c817a2df56df625ff0f84ca9776f37
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-13 11:33:40 -0500

    part 4: 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -51,6 +51,21 @@ test('a valid blog can be added', async () => {
     assert.strictEqual(blogs.body.length, helper.initialBlogs.length + 1)
 })
 
+test('if likes is missing, it defaults to 0', async ()=>{
+    const newBlog = {
+        title: 'Blog without likes',
+        author: 'Richard',
+        url: 'https://example.com'
+    } 
+
+    const response = await api.post('/api/blogs')
+    .send(newBlog).expect(201)
+
+    assert.strictEqual(response.body.likes,0)
+
+
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
